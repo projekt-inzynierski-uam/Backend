de1048e387498aed3172479dfeb468a76143eb17
Mateusz Smolarz <80050774+crabyms@users.noreply.github.com>
2023-10-24 17:08:49 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,24 @@
 import dotenv from 'dotenv'
-import pg from 'pg'
+import postgres from 'postgres'
 dotenv.config();
 
-const {Pool} = pg
-const pool = new Pool({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.DBPORT,
-    database: 'todoapp'
-})
+const pool = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
 
+async function getPostgresVersion() {
+  const response = await sql`select version()`;
+  console.log(response);
+}
+
+getPostgresVersion();
 
 export default pool
