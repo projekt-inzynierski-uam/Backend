4d017a68e7eaac1b6403081b940687e1eececd1f
Mateusz Smolarz <msmolarztech@gmail.com>
2023-10-26 00:40:51 +0200

    server changes

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-import express, { response } from 'express'
+import express from 'express'
 import cors from 'cors'
 import pool from './db.js'
 import {v4} from 'uuid'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
-const PORT = process.env.PORT || 9001
-const app = express()
-app.use(cors())
+const PORT = process.env.PORT || 5000
+const app = express();
+const corsConfig = {
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS', 'PATCH', 'PUT', 'HEAD'],
+    headers: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
+}
+
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
 app.use(express.json())
 
 //get all todos
@@ -28,8 +35,7 @@ app.post('/todos', async (req, res) => {
     const {user_email, title} = req.body
     const id = v4()
     try{
-        const newToDo = await pool.query(`INSERT INTO todos(id, user_email, title) VALUES ($1, $2, $3)`, 
-        [id, user_email, title])
+        const newToDo = await pool.query(`INSERT INTO todos(id, user_email, title) VALUES ($1, $2, $3)`, [id, user_email, title])
         res.json(newToDo)
     }catch(err){
         console.error(err)
@@ -39,11 +45,9 @@ app.post('/todos', async (req, res) => {
 //edit a todo
 app.put('/todos/:id', async(req, res) => {
     const {id} = req.params
-    const {user_email, title} = req.body
+    const {title} = req.body
     try{
-        const editToDo = 
-        await pool.query('UPDATE todos SET user_email = $1, title = $2 WHERE id = $3', 
-        [user_email, title, id])
+        const editToDo = await pool.query('UPDATE todos SET title = $1 WHERE id = $2', [title, id])
         res.json(editToDo)
     }catch(err){
         console.error(err)
@@ -54,9 +58,7 @@ app.put('/todos/:id', async(req, res) => {
 app.delete('/todos/:id', async(req, res) => {
     const {id} = req.params
     try{
-        const deleteToDo = 
-        await pool.query('DELETE FROM todos WHERE id = $1', 
-        [id])
+        const deleteToDo = await pool.query('DELETE FROM todos WHERE id = $1', [id])
         res.json(deleteToDo)
     }catch(err){
         console.error(err)
@@ -89,8 +91,7 @@ app.post('/signup', async (req, res) =>{
 app.post('/login', async (req, res) =>{
     const {email, password} = req.body
     try{
-        const users = await pool.query('SELECT * FROM users WHERE email = $1',
-        [email])
+        const users = await pool.query('SELECT * FROM users WHERE email = $1',[email])
 
         if(!users.rows.length) return res.json({detail: 'Użytkownik nie istnieje'})
 
