79d700192fc328aae1b577a8ccb372e747bbb06b
Mateusz Smolarz <80050774+crabyms@users.noreply.github.com>
2023-11-12 23:20:41 +0100

    Revert "fix (#5) (#6)" (#7)
    This reverts commit 0a3c8d93fe48f3998ef2d4e6b0598a2d4306b635.

--- data.sql
+++ data.sql
@@ -3,22 +3,10 @@ CREATE DATABASE todoapp;
 CREATE TABLE todos (
     id VARCHAR(255) PRIMARY KEY,
     user_email VARCHAR(255),
-    title VARCHAR(30),
-    assigned_to_group BOOLEAN NOT NULL
+    title VARCHAR(30)
 );
 
 CREATE TABLE users (
     email VARCHAR(255) PRIMARY KEY,
     hashed_password VARCHAR(255)
-);
-
-CREATE TABLE groups (
-    id SERIAL PRIMARY KEY,
-    group_name VARCHAR(255)
-);
-
-CREATE TABLE user_in_groups (
-    id SERIAL PRIMARY KEY,
-    user_email VARCHAR(255) REFERENCES users (email),
-    group_id INTEGER REFERENCES groups (id)
 );
