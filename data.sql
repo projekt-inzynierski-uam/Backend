bdb3c4d4ca19f329e38689e0fc493bc4631a4d23
Mateusz Smolarz <msmolarztech@gmail.com>
2023-11-12 23:43:20 +0100

    fix

--- data.sql
+++ data.sql
@@ -3,7 +3,12 @@ CREATE DATABASE todoapp;
 CREATE TABLE todos (
     id VARCHAR(255) PRIMARY KEY,
     user_email VARCHAR(255),
+<<<<<<< Updated upstream
     title VARCHAR(30)
+=======
+    title VARCHAR(30),
+    is_assigned_to_group BOOLEAN NOT NULL
+>>>>>>> Stashed changes
 );
 
 CREATE TABLE users (
