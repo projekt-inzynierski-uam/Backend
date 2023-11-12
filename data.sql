0a3c8d93fe48f3998ef2d4e6b0598a2d4306b635
Mateusz Smolarz <80050774+crabyms@users.noreply.github.com>
2023-11-12 23:07:43 +0100

    fix (#5) (#6)

--- data.sql
+++ data.sql
@@ -3,10 +3,22 @@ CREATE DATABASE todoapp;
 CREATE TABLE todos (
     id VARCHAR(255) PRIMARY KEY,
     user_email VARCHAR(255),
-    title VARCHAR(30)
+    title VARCHAR(30),
+    assigned_to_group BOOLEAN NOT NULL
 );
 
 CREATE TABLE users (
     email VARCHAR(255) PRIMARY KEY,
     hashed_password VARCHAR(255)
+);
+
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    group_name VARCHAR(255)
+);
+
+CREATE TABLE user_in_groups (
+    id SERIAL PRIMARY KEY,
+    user_email VARCHAR(255) REFERENCES users (email),
+    group_id INTEGER REFERENCES groups (id)
 );
